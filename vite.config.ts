e5327cba820975486d43d1f8e686bb6751094553
Traff <stockclothee@gmail.com>
2024-07-29 11:36:37 +0200

    add base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // base: "/web3",
+  base: "/web3",
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
   },
