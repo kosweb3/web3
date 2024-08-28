0b916968b5d9f5f33a21f195425983e2a1b156bf
kosweb3 <stockclothee@gmail.com>
2024-08-28 16:02:08 +0200

    chnages value of checkURL

--- src/components/payment/Basket.vue
+++ src/components/payment/Basket.vue
@@ -61,10 +61,10 @@ const checkUrl = computed(() => {
     return "localhost:5173";
   }
   if (window.location.href.includes("amplify")) {
-    return "https://main.d1ophk345xo11r.amplifyapp.com/";
+    return "https://main.d1ophk345xo11r.amplifyapp.com";
   }
   if (window.location.href.includes("amazonaws")) {
-    return "https://kosweb3-2024.s3.eu-north-1.amazonaws.com/";
+    return "https://kosweb3-2024.s3.eu-north-1.amazonaws.com";
   }
 });
 
@@ -78,8 +78,8 @@ const handlePayment = async (priceID) => {
     stripe.value.redirectToCheckout({
       lineItems: [{ price: priceID, quantity: 1 }],
       mode: "payment",
-      successUrl: `${checkUrl}/success-payment?token=${token}`,
-      cancelUrl: `${checkUrl}/cancel-payment`,
+      successUrl: `${checkUrl.value}/success-payment?token=${token}`,
+      cancelUrl: `${checkUrl.value}/cancel-payment`,
     });
   } catch (error) {
     console.error("Payment error:", error);
@@ -89,7 +89,6 @@ const handlePayment = async (priceID) => {
 };
 
 onMounted(async () => {
-  console.log("checkUrl", checkUrl.value);
   stripe.value = await loadStripe(import.meta.env.VITE_STRIPE_KEY);
 });
 </script>
